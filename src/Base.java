26718e3b4365c3f9b32ea3f35d495d0979e938a1
Sat_Star <satstardom@gmail.com>
2023-01-13 14:33:56 +0530

    commit2

--- src/Base.java
+++ src/Base.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Base {
     String name;
     String usn;
-    int semester;
+    int semester,a;
     double sgpa[] = new double[8];
 
     void details() {
@@ -14,6 +14,7 @@ public class Base {
         System.out.println();
         System.out.print("Enter USN : ");
         usn = sc.nextLine();
+        a =Integer.parseInt(usn.substring(7));
         System.out.println();
         System.out.print("Enter Current Semester : ");
         semester = sc.nextInt();
@@ -27,30 +28,29 @@ public class Base {
     }
 
     void proctor() {
-        if (usn.substring(7).compareTo("026") == -1)
-            System.out.println("Proctor name : Dr. Kavitha");
-        else if (usn.substring(7).compareTo("051") == -1)
-            System.out.println("Proctor name : Dr. Arbind Kumar Gupta");
-        else if (usn.substring(7).compareTo("076") == -1)
-            System.out.println("Proctor name : Dr. Roopa M.S");
-        else if (usn.substring(7).compareTo("101") == -1)
-            System.out.println("Proctor name : Dr. Amutha S");
-        else if (usn.substring(7).compareTo("126") == -1)
-            System.out.println("Proctor name : Dr. Mohammed Tajuddin");
-        else if (usn.substring(7).compareTo("151") == -1)
-            System.out.println("Proctor name : Dr. Rohini T V");
-        else if (usn.substring(7).compareTo("176") == -1)
-            System.out.println("Proctor name : Prof. Prasad A M");
-        else if (usn.substring(7).compareTo("201") == -1)
-            System.out.println("Proctor name : Dr. D Sathya");
-        else if (usn.substring(7).compareTo("226") == -1)
-            System.out.println("Proctor name : Prof. Sarala D V");
-        else if (usn.substring(7).compareTo("251") == -1)
-            System.out.println("Proctor name : Dr. Nagaraja J");
-        else if (usn.substring(7).compareTo("264") == -1)
-            System.out.println("Proctor name : Prof. Muqhita Almas");
-    }
-
+		if(a<26)
+			System.out.println("Proctor name : Dr. Kavitha");
+		else if(a<51)
+			System.out.println("Proctor name : Dr. Arbind Kumar Gupta");
+		else if(a<76)
+			System.out.println("Proctor name : Dr. Roopa M.S");
+		else if(a<101)
+			System.out.println("Proctor name : Dr. Amutha S");
+		else if(a<126)
+			System.out.println("Proctor name : Dr. Mohammed Tajuddin");
+		else if(a<151)
+			System.out.println("Proctor name : Dr. Rohini T V");
+		else if(a<176)
+			System.out.println("Proctor name : Prof. Prasad A M");
+		else if(a<201)
+			System.out.println("Proctor name : Dr. D Sathya");
+		else if(a<226)
+			System.out.println("Proctor name : Prof. Sarala D V");
+		else if(a<251)
+			System.out.println("Proctor name : Dr. Nagaraja J");
+		else if(a<276)
+			System.out.println("Proctor name : Prof. Muqhita Almas");
+	}
     void CGPA() {
         double sum = 0;
         for (int i = 1; i < semester; i++) {
